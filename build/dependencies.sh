c33f3907010cef2260e14f37a650135c44b4a6ba
Roberth Strand <me@robstr.dev>
2021-10-10 22:25:53 +0200

    Update to use args for building certain features

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes \
-  && apt-get update && apt-get install -y --no-install-recommends ca-certificates wget
+  && apt-get update && apt-get install -y --no-install-recommends ca-certificates wget curl
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
@@ -11,3 +12,12 @@ apt-get update && apt-get install -y --no-install-recommends \
 dotnet-sdk-5.0 \
 dotnet-sdk-3.1 \
 git
+
+curl -LsS https://aka.ms/InstallAzureCLIDeb | bash \
+  && rm -rf /var/lib/apt/lists/*
+
+shopt -s nocasematch
+if [[ "${TOOLS}" = "k8s" ]]
+  then
+    curl https://raw.githubusercontent.com/crayon/azdo-pipelines-agent/main/scripts/kubernetes.sh | bash
+fi
--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #~ kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /bin/
+chmod +x /bin/kubectl
 
 #~ helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
