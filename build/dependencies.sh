4bce3f7a4862a220be813a5f212efdc921574d32
Roberth Strand <me@robstr.dev>
2021-11-08 09:35:54 +0100

    Fixed kubectl issue, bump version to 2.194

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -2,26 +2,31 @@
 
 export DEBIAN_FRONTEND=noninteractive
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes \
-  && apt-get update && apt-get install -y --no-install-recommends ca-certificates wget curl
+  && apt-get update && apt-get install -y --no-install-recommends ca-certificates wget curl libicu66
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 apt-get update && apt-get install -y --no-install-recommends \
-dotnet-sdk-5.0 \
-dotnet-sdk-3.1 \
-git
+  dotnet-sdk-5.0 \
+  dotnet-sdk-3.1 \
+  git
 
+
+# AZ Cli
 curl -LsS https://aka.ms/InstallAzureCLIDeb | bash \
   && rm -rf /var/lib/apt/lists/*
 
 # Kubernetes
-if [[ ${KUBERNETES} ]];
-  then curl https://raw.githubusercontent.com/crayon/azdo-pipelines-agent/main/scripts/kubernetes.sh | bash ;
+if [[ ${KUBERNETES} ]]; then
+  echo "----"
+  echo "Installing tools for working with Kubernetes"
+  echo "----"
+  curl https://raw.githubusercontent.com/crayon/azdo-pipelines-agent/main/scripts/kubernetes.sh | bash ;
 fi
 
 # Docker
-if [ ${DOCKER} ];
-  then curl https://get.docker.com | bash ;
+if [ ${DOCKER} ]; then
+  curl https://get.docker.com | bash ;
 fi
